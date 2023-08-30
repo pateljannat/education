a1cf5eb4f1c6690ad19b87c1ef563604a4218a49
Jannat Patel <pateljannat2308@gmail.com>
2023-08-30 21:42:18 +0530

    feat: setup frappe-ui

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="max-w-3xl py-12 mx-auto">
+    Jannat here
+  </div>
+</template>
+
+<script>
+import { Dialog } from 'frappe-ui'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      showDialog: false,
+    }
+  },
+  resources: {
+    ping: {
+      url: 'ping',
+    },
+  },
+  components: {
+    Dialog,
+  },
+}
+</script>
